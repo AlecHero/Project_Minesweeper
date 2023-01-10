903b1b0f68d5a8011e43249503fff8af202ba51b
AlecHero <alexander.wittrup@gmail.com>
2023-01-10 15:18:56 +0100

    ny ma

---
+++ Ny_ma.py
@@ -0,0 +1,370 @@
+import itertools
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import pygame
+
+# ----------------------------------------Minesweeper game----------------------------------------
+# place mines
+def place_mines(mine_board):
+    
+    # generate mines number of random unique numbers between 0 and 99
+    mines = np.random.choice(ROWS*COLS, MINES, replace=False)
+    
+    for mine in mines:
+        x = mine // ROWS
+        y = mine % COLS
+        mine_board[x, y] = True 
+
+    return mine_board
+
+
+# make last layer all closed
+def reset_board():
+
+    board = np.zeros((10, ROWS, COLS), dtype=bool)
+    mine_board = np.zeros((ROWS, COLS), dtype=bool)
+    board[CLOSED] = True
+
+    mine_board = place_mines(mine_board)
+    
+    return board, mine_board
+
+
+# check for mines in the neighbours
+def count_neighbour_mines(mine_board, x, y):
+
+    min_x = max(0, x-1)
+    max_x = min(ROWS-1, x+1)
+    min_y = max(0, y-1)
+    max_y = min(COLS-1, y+1)
+    mines = mine_board[min_x:max_x+1, min_y:max_y+1]
+    return np.sum(mines)
+                    
+
+# open the neighbours
+def open_neighbour_cells(board, mine_board, x, y):
+    board[CLOSED, x, y] = False
+
+    mines = count_neighbour_mines(mine_board, x, y)
+    board[mines, x, y] = True
+
+    # if mines == 0:
+    #     for _x in range(max(0, x-1), min(ROWS, x+2)):
+    #         for _y in range(max(0, y-1), min(COLS, y+2)):
+    #             if board[CLOSED, _x, _y]:
+    #                 mines = count_neighbour_mines(mine_board, _x, _y)
+    #                 if mines == 0:
+    #                     board = open_neighbour_cells(board, mine_board, _x, _y)
+    #                 else:
+    #                     board[mines, _x, _y] = True
+    #                     board[CLOSED, _x, _y] = False 
+
+    return board
+
+
+def step(board, action, is_first_move=False):
+    x = action // ROWS
+    y = action % COLS
+    done = False
+    
+    if is_first_move and mine_board[x,y]:
+        reward = 0
+        mine_board[x,y] = 0
+        mine_board[np.argmin(mine_board[0]),0] = 1
+    elif mine_board[x, y]:
+        reward = -100
+        done = True
+    else:
+        reward = 20
+        board = open_neighbour_cells(board, mine_board, x, y)
+
+    if np.sum(board[CLOSED]) == MINES:
+        reward = 100
+        done = True
+
+    return board, reward, done
+
+def drawnow():
+    plt.gcf().canvas.draw()
+    plt.gcf().canvas.flush_events()
+plt.ion()
+
+# Define the parameters
+GAMMA = 0.99
+EPISODES = 1_000_000
+epsilon = 1.0
+epsilon_decay = 0.001
+
+# Define game parameters
+ROWS = 10
+COLS = 10
+MINES = 20
+BATCH_SIZE = 100
+
+# Define the states
+CLOSED = 9
+
+# Define the actions
+ACTIONS = ROWS * COLS
+
+# ----------------------------------------Parameters----------------------------------------
+force_cpu = True
+training = True
+
+square_size = 50
+small_square_size = 20
+slow_mode = False
+render_pygame = False
+action_line_enabled = False
+one_hot_board = False
+pp = 0
+
+if one_hot_board:
+    pp = 25
+
+if not force_cpu:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+else:
+    device = torch.device("cpu")
+
+from torch.utils.data import Dataset, DataLoader
+
+# create a buffer for state, action, new_action, reward, done
+buffer_size = 100
+state_buffer = np.zeros((buffer_size, 10, ROWS, COLS), dtype=bool)
+action_buffer = np.zeros(buffer_size, dtype=int)
+new_state_buffer = np.zeros((buffer_size, 10, ROWS, COLS), dtype=bool)
+new_action_buffer = np.zeros(buffer_size, dtype=int)
+reward_buffer = np.zeros(buffer_size, dtype=int)
+done_buffer = np.zeros(buffer_size, dtype=bool)
+
+# Define the network CNN with a kernel size of 5
+model = nn.Sequential(
+    nn.Conv2d(10, 32, kernel_size=5, stride=1, padding=1),
+    nn.Sigmoid(),
+    nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=1),
+    nn.Sigmoid(),
+    nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=1),
+    nn.Sigmoid(), 
+    nn.Flatten(),
+    nn.Linear(2048, ACTIONS) 
+).to(device)
+print(device)
+
+# load the model if it exists
+try:
+    model.load_state_dict(torch.load("Baba1.pth"))
+    print("Model loaded")
+except FileNotFoundError:
+    print("Model not found")
+
+# Define the optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+
+# Define the loss function
+loss_fn = nn.MSELoss()
+scores = []
+losses = []
+action_line = []
+step_count = 0
+
+pygame.init()
+
+# make space for 10 small boards on the right side of the big board in the center
+if one_hot_board:
+    board_width = (ROWS) * square_size * 3.5 - pp
+    board_height = (COLS) * square_size + 2 * pp
+else:
+    board_width = (ROWS) * square_size
+    board_height = (COLS) * square_size
+
+screen = pygame.display.set_mode((board_width, board_height))
+
+
+done_counter = 0
+lost_counter = 0
+
+
+def play_game():
+    screen.fill((255, 255, 255))
+
+    # undo one hot encoding and draw the board
+    game_board = np.argmax(state, axis=0)
+
+
+    for y in range(ROWS):
+        for x in range(COLS):
+            if state[CLOSED, y, x] == 1:
+                pygame.draw.rect(screen, (144, 238, 144), (pp + x * square_size, pp + y * square_size, square_size, square_size))
+            elif state[0, y, x] == 1:
+                pygame.draw.rect(screen, (255, 255, 255), (pp + x * square_size, pp + y * square_size, square_size, square_size))
+            else:
+                pygame.draw.rect(screen, (255, 255, 255), (pp + x * square_size, pp + y * square_size, square_size, square_size))
+                text = str(game_board[y, x].item())
+                font = pygame.font.SysFont('Arial', 30)
+                text_surface = font.render(text, True, (0, 0, 0))
+                screen.blit(text_surface, (pp + x * square_size + 20, pp + y * square_size + 10))
+
+            if mine_board[y, x] == True:
+                pygame.draw.rect(screen, (255, 0, 0), (pp + x * square_size + 10, pp + y * square_size + 10, square_size - 20, square_size - 20))
+
+            if y == int(action // ROWS) and x == int(action % COLS):
+                pygame.draw.rect(screen, (0, 0, 255), (pp + x * square_size + 5, pp + y * square_size + 5, 10, 10))
+
+    # render the grid
+    for x in range(ROWS + 1):
+        pygame.draw.line(screen, (0, 0, 0), (pp + x * square_size, pp), (pp + x * square_size, pp + COLS * square_size))
+    for y in range(COLS + 1):
+        pygame.draw.line(screen, (0, 0, 0), (pp, pp + y * square_size), (pp + ROWS * square_size, pp + y * square_size))
+
+    
+    # draw a line from each action to the next
+    if action_line_enabled:
+        action_line.append(action)
+        for i in range(len(action_line) - 1):
+            pygame.draw.line(screen, (0, 0, 255), (pp + int(action_line[i] % ROWS) * square_size + 5, pp + int(action_line[i] // COLS) * square_size + 5), (pp + int(action_line[i + 1] % ROWS) * square_size + 5, pp + int(action_line[i + 1] // COLS) * square_size + 5))
+    
+    
+    if one_hot_board:
+        # draw the 10 small boards from one hot encoding 5 wide and 2 tall to the right of the big board start from the top left corner
+        for i in range(10):
+            for x, y in itertools.product(range(ROWS), range(COLS)):
+                if state[i, x, y] == 1:
+                    pygame.draw.rect(screen, (102, 153, 204), (ROWS * square_size - 110 + (i % 5 + 1) * (ROWS + 1) * small_square_size + x * small_square_size, 50 + (i // 5) * (COLS + 1) * small_square_size + y * small_square_size, small_square_size, small_square_size))
+                else:
+                    pygame.draw.rect(screen, (255, 255, 255), (ROWS * square_size - 110 + (i % 5 + 1) * (ROWS + 1) * small_square_size + x * small_square_size, 50 + (i // 5) * (COLS + 1) * small_square_size + y * small_square_size, small_square_size, small_square_size))
+
+            # render the grid
+            for x in range(ROWS + 1):
+                pygame.draw.line(screen, (0, 0, 0), (ROWS * square_size - 110 + (i % 5 + 1) * (ROWS + 1) * small_square_size + x * small_square_size, 50 + (i // 5) * (COLS + 1) * small_square_size), (ROWS * square_size - 110 + (i % 5 + 1) * (ROWS + 1) * small_square_size + x * small_square_size, 50 + (i // 5) * (COLS + 1) * small_square_size + COLS * small_square_size))
+            for y in range(COLS + 1):
+                pygame.draw.line(screen, (0, 0, 0), (ROWS * square_size - 110 + (i % 5 + 1) * (ROWS + 1) * small_square_size, 50 + (i // 5) * (COLS + 1) * small_square_size + y * small_square_size), (ROWS * square_size - 110 + (i % 5 + 1) * (ROWS + 1) * small_square_size + ROWS * small_square_size, 50 + (i // 5) * (COLS + 1) * small_square_size + y * small_square_size))
+    
+    
+    pygame.display.update()
+    if slow_mode:
+        pygame.time.delay(200)
+
+
+def model_action(state):
+    invalid_actions = np.logical_not(state[CLOSED].flatten()).nonzero()[0]
+                
+    observation = model(torch.from_numpy(np.expand_dims(state, axis=0)).float().to(device))
+    observation = observation.detach().cpu().numpy().flatten()
+    observation[invalid_actions] = -np.inf
+
+    return np.argmax(observation)
+
+
+#----------------------------------------Training----------------------------------------
+for episode in range(EPISODES):
+    epsilon = max(epsilon - epsilon_decay, 0.001)
+
+    state, mine_board = reset_board()
+    score = 0
+    done = False
+    is_first_move = True
+    
+    while not done:
+        step_count += 1
+    
+        if np.random.rand() < epsilon:
+            valid_actions = state[CLOSED].flatten().nonzero()[0]
+            action = np.random.choice(valid_actions)   
+        else:
+            action = model_action(state)
+        
+        if render_pygame:
+            play_game()
+            
+        new_state, reward, done = step(state, action, is_first_move=is_first_move)
+        is_first_move = False
+        score += reward
+        
+        new_action = model_action(new_state)
+
+        #BUFFER
+        buffer_idx = step_count % buffer_size
+        state_buffer[buffer_idx] = state
+        action_buffer[buffer_idx] = action
+        new_state_buffer[buffer_idx] = new_state
+        new_action_buffer[buffer_idx] = new_action
+        reward_buffer[buffer_idx] = reward
+        done_buffer[buffer_idx] = done
+            
+        state = new_state
+
+    # Train the model
+    if step_count > buffer_size:
+        batch_idx = np.random.choice(buffer_size, size=BATCH_SIZE)
+        
+        out = model(torch.tensor(state_buffer[batch_idx]).float().to(device))
+        q_val = out[np.arange(BATCH_SIZE), action_buffer[batch_idx]]
+        out_next = model(torch.tensor(new_state_buffer[batch_idx]).float().to(device))
+        q_val_next = out_next[np.arange(BATCH_SIZE), new_action_buffer[batch_idx]]
+
+        target = torch.tensor(reward_buffer[batch_idx]).float().to(device) + GAMMA * q_val_next * (1 - torch.tensor(done_buffer[batch_idx]).float().to(device))
+        l = loss_fn(q_val, target.float())
+
+        optimizer.zero_grad()
+        l.backward()
+        optimizer.step()
+
+    scores.append(score)
+    
+    if step_count > buffer_size:
+        losses.append(l.item())
+    
+    if episode % 10 == 0:
+        avg_score = np.mean(scores[-1000:])
+        print(f"episode {episode:>5}, score {score:>6.1f}, avg score {avg_score:>6.1f}, eps {epsilon:.3f}, losses {np.mean(losses[-1000:]):.3f}")
+        
+        # plt.clf()
+        # plt.plot(scores)
+        # plt.title(f'Episode {episode}, score {score}, avg score {avg_score}, eps {epsilon:.3f}')
+        # drawnow()
+        
+    if action_line_enabled:
+        action_line = []
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+            training = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
+            slow_mode = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+            slow_mode = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            render_pygame = not render_pygame
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_l:
+            action_line_enabled = not action_line_enabled
+
+    if not training:
+        break
+
+torch.save(model.state_dict(), 'Baba1.pth')
+# plot learning curve
+x = [i+1 for i in range(len(scores))]
+running_avg = np.zeros(len(scores))
+for i in range(len(running_avg)):
+    running_avg[i] = np.mean(scores[max(0, i-1000):(i+1)])
+    
+# make a trend line
+z = np.polyfit(x, running_avg, 1)
+p = np.poly1d(z)
+
+# show formula for trend line on plot
+plt.plot(x, running_avg)
+plt.plot(x,p(x),"r")  
+plt.legend([f"y={z[0]:.20f}x+{z[1]:.2f}"])
+plt.title("Learning Curve")
+plt.xlabel("Episode")
+plt.ylabel("Score")
+plt.show()
+plt.waitforbuttonpress()
